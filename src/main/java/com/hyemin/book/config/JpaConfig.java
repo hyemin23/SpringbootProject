ac209f31154e91112042dde33c4d09c0a4da7d88
hyemin23 <hm_stom@naver.com>
2020-02-22 17:46:28 +0900

    #build.gradle 수정

---
+++ src/main/java/com/hyemin/book/config/JpaConfig.java
@@ -0,0 +1,10 @@
+package com.hyemin.book.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing  //JPA Auditing활성화
+public class JpaConfig {
+
+}
